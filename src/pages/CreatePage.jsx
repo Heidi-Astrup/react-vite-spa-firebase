bf58c4b85e40d7257c3b0a4f8a94f92706438b2a
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-09-29 14:02:18 +0200

    Oprettet komponenter

---
+++ src/pages/CreatePage.jsx
@@ -0,0 +1,9 @@
+export default function CreatePage() {
+  return (
+    <section className="page">
+      <h1>Create Page</h1>
+      <p>Opret noget nyt</p>
+      <p>I am one arm away 🤷🏼‍♂️</p>
+    </section>
+  );
+}
